1db1bc716bb91fd06d176aee506455001bfe9801
Reng Tsai <rengah0@gmail.com>
2020-04-15 19:01:12 +0800

    update

--- src/js/interact.js
+++ src/js/interact.js
@@ -1,18 +1,23 @@
-var cur_page='HOME';
+var _cur_page='HOME';
+var _driver_name;
+var _driver_color='blue';
 
 function gotoPage(page_){
 	
-	if(cur_page===page_) return;
+	if(_cur_page===page_) return;
 
 	switch(page_){
 		case '_home':
 			showItem($('#_button_rank'));
+			$('#_input_driver').val("");
+			_driver_name="";
 			break;
 		case '_driver':
-			hideItem($('#_button_rank'));
+			hideItem($('#_button_rank'));			
 			$('#_input_driver').focus();
 			break;
 		case '_color':
+			setDriverColor('blue');
 			hideItem($('#_button_rank'));
 			break;
 		case '_game':
@@ -26,10 +31,10 @@ function gotoPage(page_){
 			break;
 	}
 
-	hideItem($('#'+cur_page));
+	hideItem($('#'+_cur_page));
 	showItem($('#'+page_));
 
-	cur_page=page_;
+	_cur_page=page_;
 
 }
 
@@ -62,7 +67,31 @@ function showItem(item_){
 	},10);
 }
 
-function setCarColor(set_){
+function onDriverNameClick(){
+
+	let val=$('#_input_driver').val();
+	if(val.length<1) return;
+
+	setDriverName(val);
+	gotoPage('_color');
+}
+
+function setDriverName(set_){
+	_driver_name=set_;
+	$('#_driver_name_in_game').text(_driver_name);
+	$('#_driver_name_complete').text(_driver_name);
+}
+function setDriverColor(set_){
+
+	if(set_===_driver_color) return;
+
+	hideItem($('#_button_'+_driver_color+'_selected'));
+	hideItem($('#_img_car_color_'+_driver_color));
+	
+	showItem($('#_button_'+set_+'_selected'));
+	showItem($('#_img_car_color_'+set_));
+	
+	_driver_color=set_;
 
 }
 
